3d113c611f15e536c9de54ca18bba657570a27a2
Alexis Duran <alexis.duranm@gmail.com>
2020-10-13 16:32:13 +0200

    build(eslint): remove some possible uneeded envs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,6 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     env: {
         browser: true,
-        commonjs: true,
-        es6: true,
-        node: true,
         jest: true
     },
     extends: [
