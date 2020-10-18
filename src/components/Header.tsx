3f79ca63edd08c5ac0f821c6dfd7005edbc7413d
Alexis Duran <alexis.duranm@gmail.com>
2020-10-18 16:26:31 +0200

    refactor(header): use isolated component for nav link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,26 @@
 import React from 'react'
 
+const NavLink = ({ children, preffix, ...linkAttrs }) => {
+    return (
+        <a className="px-4 text-2xl font-bold uppercase" {...linkAttrs}>
+            <p className="top-0 left-0 block m-0 text-xs italic font-normal">
+                {preffix}
+            </p>
+            <p className="relative block leading-none">{children}</p>
+        </a>
+    )
+}
+
 const Header = () => {
     return (
         <header className="p-4">
             <nav className="flex justify-end p-4">
-                <a
-                    className="relative ml-4 text-4xl font-bold leading-loose first:ml-0"
-                    href="#"
-                >
-                    <span className="absolute top-0 left-0 block text-xs italic font-normal font-cursive">
-                        Get my
-                    </span>
-                    RESUME
-                </a>
-                <a
-                    className="relative ml-4 text-4xl font-bold leading-loose first:ml-0"
-                    href="mailto:hi@alexisduran.com"
-                >
-                    <span className="absolute top-0 left-0 block text-xs italic font-normal font-cursive">
-                        Let&apos;s work
-                    </span>
-                    CONTACT
-                </a>
+                <NavLink href="#" preffix="Get my">
+                    resume
+                </NavLink>
+                <NavLink href="mailto:hi@alexisduran.com" preffix="Let's work">
+                    mail me
+                </NavLink>
             </nav>
         </header>
     )
