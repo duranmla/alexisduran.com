d51b3f41a196b9ef0b898adaf14f26cfb31a7d1e
Alexis Duran <alexis.duranm@gmail.com>
2020-11-06 16:25:51 +0100

    feat(menu): allow to toggle menu icon open/close

--- src/components/MenuIcon.tsx
+++ src/components/MenuIcon.tsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import classNames from 'classnames'
 
 const MenuIcon = () => {
+    const [open, setOpen] = useState(false)
+    const ariaLabel = open ? 'open menu' : 'close menu'
+    const classes = classNames(
+        'flex flex-col content-center justify-center w-10 h-10 m-4 ml-auto bg-white rounded-full shadow',
+        { open }
+    )
+    const handleOnClick = () => {
+        setOpen((state) => !state)
+    }
+
     return (
         <button
-            className="flex flex-col content-center justify-center w-10 h-10 m-4 ml-auto bg-white rounded-full shadow"
-            aria-label="open menu"
+            onClick={handleOnClick}
+            className={classes}
+            aria-label={ariaLabel}
         >
             <div className="mx-auto stripe-menu-icon">
                 <div className="stripe" />
                 <div className="stripe" />
                 <div className="stripe" />
+                <div className="stripe-close" />
             </div>
         </button>
     )
