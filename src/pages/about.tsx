e30a3fa36150cc352f28bf75cef7d2e0224e3bce
Alexis Duran <alexis.duranm@gmail.com>
2020-12-06 14:36:03 +0100

    feat(about): add back to homepage for mobile

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Layout } from '../components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronCircleLeft } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'gatsby'
 
-const IndexPage = ({ data }) => (
+const AboutPage = () => (
     <Layout>
         <section className="flex flex-col items-center">
             <div className="container px-8">
+                <nav className="flex mb-8 sm:hidden">
+                    <Link to="/" className="text-xs italic">
+                        <FontAwesomeIcon icon={faChevronCircleLeft} /> Back to
+                        homepage
+                    </Link>
+                </nav>
                 <small className="text-xl leading-none">Hola! I&apos;m</small>
                 <h1 className="text-4xl font-bold leading-normal">
                     Alexis Duran
@@ -25,4 +34,4 @@ const IndexPage = ({ data }) => (
     </Layout>
 )
 
-export default IndexPage
+export default AboutPage
