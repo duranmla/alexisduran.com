e800903226541af85f0d94d1d83837b0dd186a7d
Alexis Duran <alexis.duranm@gmail.com>
2020-12-08 12:50:14 +0100

    fix(technologies): list spacing

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -13,7 +13,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const Technologies = ({ className = '' }) => {
     return (
         <div className={className}>
-            <ul className="flex flex-row flex-wrap content-start justify-between -ml-6 space-x-6 space-y-4">
+            <ul className="flex flex-row flex-wrap justify-start -ml-6 space-x-6 space-y-4">
                 <li className="flex flex-col items-center mt-4 ml-6 space-y-2">
                     <FontAwesomeIcon className="text-3xl" icon={faReact} />
                     <small>React</small>
