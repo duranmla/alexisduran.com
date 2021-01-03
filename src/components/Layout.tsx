eb1dcc6a558a31a95bf240ee3bd213817deb1176
Alexis Duran <alexis.duranm@gmail.com>
2021-01-03 16:02:58 +0100

    fix(layout): update meta viewport tag

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ const Layout = ({ children, className = '' }) => (
         <Helmet>
             <meta
                 name="viewport"
-                content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+                content="viewport-fit=cover, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
             />
         </Helmet>
         <div className={`layout ${className}`}>
