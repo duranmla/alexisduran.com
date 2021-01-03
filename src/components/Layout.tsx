43759f91b580c5099ffb42496cb92a3153f08e34
Alexis Duran <alexis.duranm@gmail.com>
2021-01-03 15:40:27 +0100

    fix(layout): avoid zoom out on mobile

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import Footer from './Footer'
 import Header from './Header'
 
 const Layout = ({ children, className = '' }) => (
-    <div className={`layout ${className}`}>
-        <Header />
-        {children}
-        <Footer />
-    </div>
+    <>
+        <Helmet>
+            <meta
+                name="viewport"
+                content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+            />
+        </Helmet>
+        <div className={`layout ${className}`}>
+            <Header />
+            {children}
+            <Footer />
+        </div>
+    </>
 )
 
 export default Layout
