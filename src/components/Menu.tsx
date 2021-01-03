d47a9fe159b279deeb9c36c9421207aee2b928b7
Alexis Duran <alexis.duranm@gmail.com>
2021-01-03 16:03:30 +0100

    fix(menu): remove bottom margin on menu mobile

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -55,7 +55,7 @@ const Menu = () => {
                     About
                 </Link>
             </nav>
-            <div className="absolute bottom-0 right-0 w-full p-8 mb-16 text-center sm:block">
+            <div className="absolute bottom-0 right-0 w-full p-8 text-center sm:block">
                 <p className="mb-2 italic">Let&apos;s connect!</p>
                 <SocialMedia orientation="row" />
             </div>
