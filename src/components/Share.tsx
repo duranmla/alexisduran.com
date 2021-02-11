622cd79bd63103cf0120933ca0453f06c75e7528
Alexis Duran <alexis.duranm@gmail.com>
2021-02-11 12:52:59 +0100

    feat(blog): add twitter share component
    besides, this commit will adjust width for blog post template

---
+++ src/components/Share.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+const Share = () => {
+    return (
+        <div className="relative max-w-full p-8 mx-auto share-container">
+            <p className="pl-24 mb-12 md:pl-0">
+                If you take any value from this post or you have any comment I
+                would love to know about it. Start a conversation or simple{' '}
+                <span className="italic font-bold underline">
+                    share on Twitter
+                </span>
+            </p>
+            <p className="hidden text-2xl font-bold md:block">Click the bird</p>
+            <FontAwesomeIcon
+                className="share-icon"
+                icon={faTwitter}
+                size="lg"
+                color="white"
+            />
+            <div
+                className="absolute hidden md:block"
+                style={{ width: '20rem', bottom: '25px', left: '-75px' }}
+            >
+                <svg
+                    viewBox="0 0 310 129"
+                    fill="none"
+                    style={{ width: '100%' }}
+                >
+                    <path
+                        d="M10.9756 5C31.3756 18.3237 42.2813 22.3973 46.3024 42.0904C51.5051 67.5697 18.9366 75 6.00002 96.4577C-15.9765 132.91 85.5 124.914 108 124.914H310M10.9756 5C16.4488 12.6215 18.9366 26.9662 18.9366 26.9662M10.9756 5C22.5 8.5 38 7 38 7"
+                        stroke="#fff"
+                        strokeWidth="3"
+                    ></path>
+                </svg>
+            </div>
+        </div>
+    )
+}
+
+export default Share
--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -5,6 +5,7 @@ import { faChevronCircleLeft } from '@fortawesome/free-solid-svg-icons'
 import SEO from '../components/SEO'
 import { Layout } from '../components'
 import { defineCustomElements as deckDeckGoHighlightElement } from '@deckdeckgo/highlight-code/dist/loader'
+import Share from '../components/Share'
 
 deckDeckGoHighlightElement()
 
@@ -20,8 +21,8 @@ export default function BlogPost({ data }) {
                 pageUrl="http://alexisduran.com/about"
             />
             <Layout>
-                <section className="flex flex-col items-center space-y-2">
-                    <div className="container px-8">
+                <div className="flex flex-col items-center space-y-2">
+                    <div className="container max-w-3xl px-8">
                         <nav className="flex my-8 sm:hidden">
                             <Link to="/blog" className="text-xs italic">
                                 <FontAwesomeIcon icon={faChevronCircleLeft} />{' '}
@@ -35,11 +36,30 @@ export default function BlogPost({ data }) {
                             {post.frontmatter.date}
                         </h2>
                         <section
-                            className="mb-32 content post-content"
+                            className="content post-content"
                             dangerouslySetInnerHTML={{ __html: post.html }}
                         />
                     </div>
-                </section>
+                </div>
+                <div className="lg:block">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 1440 320"
+                    >
+                        <path
+                            fill="rgb(229,62,62, .5)"
+                            d="M0,64L24,101.3C48,139,96,213,144,208C192,203,240,117,288,90.7C336,64,384,96,432,133.3C480,171,528,213,576,234.7C624,256,672,256,720,245.3C768,235,816,213,864,202.7C912,192,960,192,1008,181.3C1056,171,1104,149,1152,170.7C1200,192,1248,256,1296,240C1344,224,1392,128,1416,80L1440,32L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z"
+                        ></path>
+                    </svg>
+                </div>
+                <div
+                    className="py-8 pb-32"
+                    style={{ background: 'rgb(229,62,62, .5)' }}
+                >
+                    <div className="container px-8 mx-auto">
+                        <Share />
+                    </div>
+                </div>
             </Layout>
         </>
     )
